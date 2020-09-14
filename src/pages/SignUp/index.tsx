0115a7681ed254320355b00953405ff4b0923293
Gustavo Harff <gustavo.harff@gmail.com>
2020-09-14 17:16:37 -0300

    chore: auto correct ajust

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -96,6 +96,7 @@ const SignUp: React.FC = () => {
             <Form ref={formRef} onSubmit={handleSignUp}>
               <Input
                 autoCapitalize="words"
+                autoCorrect={false}
                 name="name"
                 icon="user"
                 placeholder="Nome"
